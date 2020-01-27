7d37d7bd3c3da9ce323f2991e8f3fe70f292cd4c
fireinjun <mk@fireinjun.com>
2020-01-27 10:55:04 -0500

    finished boilerplate

---
+++ src/calculate_average_loss/calculate_averageloss.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3import sys
+import sys
+
+
+class UserMainCode(object):
+    @classmethod
+    def calculate_loss_rate(cls, input1, input2)
+    '''
+    input1 : int
+    input2 : int
+
+    Expected return type : int
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv[1]) < 2:
+        input1 = int(input())
+        input2 = int(input())
+else:
+    input1 = str(sys.argv[1].strip())
