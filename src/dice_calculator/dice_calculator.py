0e734be0f46e3d486978ee78e580536114a847ea
fireinjun <mk@fireinjun.com>
2020-01-27 10:34:30 -0500

    Added starter code to dice

---
+++ src/dice_calculator/dice_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3import sys
+
+import sys
+
+
+class UserMainCode(object):
+    @classmethod
+    def dice_calculator(cls, input1)
+    '''
+    input1 : string
+
+    Expected return type : int
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv[1]) < 2:
+        input1 = str(input().strip())
+else:
+    input1 = str(sys.argv[1].strip())
